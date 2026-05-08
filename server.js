89446f5348d464e2924822c7e2c0fefea6d2a220
Daniel Gaddis <daniel.gaddis0@gmail.com>
2026-05-08 11:11:16 -0500

    Commented out local dev dns settings for postman testing. Ready to deploy to Render after this.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const dns = require('node:dns');
-dns.setServers(['8.8.8.8', '1.1.1.1']);
+/*
+const dns = require('node:dns'); for local development
+dns.setServers(['8.8.8.8', '1.1.1.1']); for local development
+*/
 
 require('dotenv').config();
 
